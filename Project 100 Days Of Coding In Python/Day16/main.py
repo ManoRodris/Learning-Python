4d9bea3b44a0625b810bdfcb964fffc490887b3e
Rodrigo Dórea <rodrigodoreafelix@gmail.com>
2024-08-19 23:15:18 -0300

    Learning POO and the basic of libraries

---
+++ Project 100 Days Of Coding In Python/Day16/main.py
@@ -0,0 +1,22 @@
+#Testing some libraries
+
+"""from turtle import Turtle, Screen
+
+timmy = Turtle()
+
+timmy.shape("turtle")
+timmy.color("cyan2")
+timmy.left(120)
+timmy.forward(100)
+
+screen = Screen()
+screen.exitonclick()"""
+
+import prettytable
+
+pokemon_table = prettytable.PrettyTable()
+
+pokemon_table.add_column("Pokemon", ["Charmander", "Bublasaur", "Squirtle"])
+pokemon_table.add_column("Type", ["Fire", "Grass", "Water"])
+pokemon_table.align = "l"
+print(pokemon_table)
