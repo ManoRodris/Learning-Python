bec1687be2fb4d4990ce89161eb729e47be022b1
Rodrigo Dórea <rodrigodoreafelix@gmail.com>
2024-11-05 23:56:38 -0300

    update

--- Project 100 Days Of Coding In Python/Day25/average_temp.py
+++ Project 100 Days Of Coding In Python/Day25/average_temp.py
@@ -3,4 +3,9 @@ import pandas
 weather_data = pandas.read_csv("weather_data.csv")
 temp_list = weather_data["temp"].mean()
 max_value = weather_data["temp"].max()
-print(max_value)
+#print(max_value)
+#print(weather_data[weather_data.temp == max_value])
+
+monday = weather_data[weather_data.day == "Monday"]
+#fahrenheit_monday = monday.temp -
+print(type(monday.temp))
