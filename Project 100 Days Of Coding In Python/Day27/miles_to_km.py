b5e7a1ee3f97de9054b4676d004349defa7ccca8
Rodrigo <rodrigodoreafelix@gmail.com>
2025-02-12 15:10:28 -0300

    Finishing the miles to km converter

--- Project 100 Days Of Coding In Python/Day27/miles_to_km.py
+++ Project 100 Days Of Coding In Python/Day27/miles_to_km.py
@@ -1,19 +1,37 @@
 from tkinter import *
 
+def miles_to_km():
+    m = float(entry.get())
+    km = round(m * 1.609, 2)
+    km = str(km)
+    label_3.config(text=f"{km}")
+
 # Window
 window = Tk()
 window.title("Miles to Km Converter")
-window.minsize(width=300, height=300)
+window.minsize(width=200, height=100)
+window.config(padx=20, pady=20)
 
 # Entry
 entry = Entry(width=10)
-entry.grid(column=1, row=0, padx=5, pady=(15, 0))
+entry.insert(0, string="0")
+entry.grid(column=1, row=0)
 
 # Labels
 label = Label(text="Miles")
-label.grid(column=2, row=0, pady=(15, 0))
+label.grid(column=2, row=0)
 
 label_2 = Label(text="is equal to")
 label_2.grid(column=0, row=1)
+
+label_3 = Label(text="0")
+label_3.grid(column=1, row=1)
+
+label_4 = Label(text="Km")
+label_4.grid(column=2, row=1)
+
+# Button
+button = Button(text="Calculate", command=miles_to_km)
+button.grid(column=1, row=2)
 
 window.mainloop()
