48250e4cb9e073d1962fd75d71952858d1ddb89e
Rodrigo Dórea <rodrigodoreafelix@gmail.com>
2025-03-22 21:34:49 -0300

    update

--- Project 100 Days Of Coding In Python/Day28/main.py
+++ Project 100 Days Of Coding In Python/Day28/main.py
@@ -7,14 +7,27 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 0.2
-SHORT_BREAK_MIN = 0.1
-LONG_BREAK_MIN = 1
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
 REPS = 0
+timer = None
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+   window.after_cancel(timer)
+   canvas.itemconfig(timer_text, text="00:00")
+   title_label.config(text="Timer")
+   checkmark_label.config(text="")
+   global REPS
+   REPS = 0
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def amount_of_work(label):
+   current_text = label.cget("text")
+   new_text = f"{current_text}✔"
+   label.config(text=new_text)
+
 def start_timer():
    global REPS
 
@@ -23,14 +36,17 @@ def start_timer():
    long_break_time = LONG_BREAK_MIN * 60
 
    if REPS in (0, 2, 4, 6):
+      title_label.config(text="Work", fg=GREEN)
       count_down(work_time)
       REPS += 1
    elif REPS in (1, 3, 5):
+      title_label.config(text="Break", fg=PINK)
       count_down(short_break_time)
       REPS += 1
    elif REPS == 7:
+      title_label.config(text="Break", fg=RED)
       count_down(long_break_time)
-      REPS == 0
+      REPS = 0
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -44,9 +60,13 @@ def count_down(count):
       canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
 
    if count > 0:
-      window.after(1000, count_down, count - 1)
+      global timer
+      timer = window.after(1000, count_down, count - 1)
    else:
       start_timer()
+      if REPS in (0, 2, 4, 6):
+         amount_of_work(checkmark_label)
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 # Configuring the window interface
@@ -59,7 +79,7 @@ title_label = Label(text="Timer", font=(FONT_NAME, 35), fg=GREEN, bg=YELLOW)
 title_label.grid(column=1, row=1)
 
 # Configuring the tomato visualization in canva
-tomato_img = PhotoImage(file=r"C:\Users\Rodrigo\Documents\Projects\Learning-Python\Project 100 Days Of Coding In Python\Day28\tomato.png")
+tomato_img = PhotoImage(file="tomato.png")
 canvas = Canvas(window, width=220, height=240, bg=YELLOW, highlightthickness=0)
 canvas.create_image(110, 120, image=tomato_img)
 timer_text = canvas.create_text(110, 140, text="00:00", font=(FONT_NAME, 25, "bold"), fill="white")
@@ -69,11 +89,11 @@ canvas.grid(column=1, row=2)
 start_button = Button(text="Start", bg=YELLOW, fg=RED, command=start_timer)
 start_button.grid(column=0, row=3)
 
-reset_button = Button(text="Reset", bg=YELLOW, fg=RED)
+reset_button = Button(text="Reset", bg=YELLOW, fg=RED, command=reset_timer)
 reset_button.grid(column=2, row=3)
 
 # Configuring the amount of times the pomodoro app was played
-checkmark_label = Label(text="✔", font=(FONT_NAME, 16), fg=GREEN, bg=YELLOW)
+checkmark_label = Label(text="", font=(FONT_NAME, 16), fg=GREEN, bg=YELLOW)
 checkmark_label.grid(column=1, row=4)
 
 window.mainloop()
